a006169317ceaec2b39352c205fe60fd3e29315d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-03 13:52:03 +0100

    push and create

--- stack.c
+++ stack.c
@@ -23,4 +23,18 @@ void create (Stack *ptrS, int a){
     ptrS->top = NULL;
 }
 //push – add element to stack;
+void push(int a, Stack* ptrS){
+    if( (ptrS->currentSize+1) <= (ptrS->maxSize) ){
+        Element* newE = (Element*) calloc (1, sizeof(Element));
+        newE->data = a;
+        if(ptrS->top != NULL){ //checks if it isn't the first element in the stack
+            newE->previous = ptrS->top;
+        }else{
+            ptrS->top = newE;
+            newE->previous = NULL;
+        }        
+        newE->next = NULL;
+        ptrS->currentSize++;
+    }
+}
 
